39d2a6e8f76f7c224f94962cffb222a0e27a9b87
Keith <35976948+fivepieces@users.noreply.github.com>
2025-04-09 18:52:10 -0400

    Timer and Image update
    moved button down a little, made image swap when in study mode

--- pomodoro-timer/src/index.js
+++ pomodoro-timer/src/index.js
@@ -1,23 +1,35 @@
 let countdown;
 
 function startTimer() {
-  clearInterval(countdown);
-  const timerDisplay = document.getElementById("timer");
-  let timeLeft = 60;
 
-  countdown = setInterval(() => {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
-      .toString()
-      .padStart(2, "0")}`;
+    // Hide Start button
+    document.getElementById("startBtn").style.display = 'none'
 
-    if (timeLeft <= 0) {
-      clearInterval(countdown);
-    }
+    // Change chicken image to study mode
+    const chicken = document.getElementById('chickenImg');
+    chicken.src ="../assets/ChickenStudy.png";
 
-    timeLeft--;
-  }, 1000);
+    // Show the timer
+    document.getElementById("timer").style.display = 'block';
+
+    //Reset and start the countdown
+    clearInterval(countdown);
+    const timerDisplay = document.getElementById("timer");
+    let timeLeft = 60;
+
+    countdown = setInterval(() => {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+        timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
+        .toString()
+        .padStart(2, "0")}`;
+
+        if (timeLeft <= 0) {
+        clearInterval(countdown);
+        }
+
+        timeLeft--;
+    }, 1000);
 }
 
 // Add event listeners for custom window controls
